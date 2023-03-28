dd82175a00b65be5053e98bd3bbd8b80a310976c
세순 송 <sss010201sss@naver.com>
2023-03-28 14:38:43 +0900

    2023.03.28

--- BassicClass/MainApp.cs
+++ BassicClass/MainApp.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BassicClass
 {
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            Console.WriteLine($"{Name} : 야옹");
+        }
+    }
+    
     class MainApp
     {
-        static void Main(string[] args)
+        static void Main(string args[])
         {
+            Cat kitty = new BassicClass.Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+            Console.WriteLine($"{kitty.Name}:{kitty.Color}");
+
+            Cat nero = new BassicClass.Cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+            nero.Meow();
+            Console.WriteLine($"{nero.Name}: {nero.Color}");
         }
     }
 }
