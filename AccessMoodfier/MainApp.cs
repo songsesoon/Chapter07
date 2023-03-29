15eb1e17b1779c64aab348b1af157fff366c0727
세순 송 <sss010201sss@naver.com>
2023-03-29 15:53:51 +0900

    2023.03.29

---
+++ AccessMoodfier/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AccessMoodfier
+{
+    class WaterHeater
+    {
+        projected int temperaature;
+
+        public void SetTemperature(int temperature;
+        {
+            if (temperature< -5 || temperature> 42 )
+            {
+                throw new Exception("Out of temperature range");
+            }
+           
+            this.temperature= temperature;
+        }
+    
+        internal void TurnOnWater()
+        {
+             Console.WriteLine($"Turn On Water : {temperature}");
+        }
+    }
+class MainApp
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            WaterHeater heater = new WaterHeater();
+            heater.SetTemperature(29);
+            heater.TurnOnWater();
+
+            heater.SetTemperature(-2);
+            heater.TurnOnWater();
+        }
+        catch (Exception e0
+        {
+            Console.WriteLine(e.Message)
+        }
+    }
+}
+
+       
---
+++ This/MainApp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace This
+{
+    class Emplogyee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetPosition(string Position);
+        {
+             this.Position=Position;
+        }
+
+        public string GetPosition()
+        {
+             return this.GetPosition;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Emplogyee pooh = new Emplogyee();
+            pooh.SetName("Pooh");
+            pooh SetPosition("Water");
+            Console.WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Emplogyee tigger = new Emplogyee();
+            tigger.SetName("Tigger");
+            tigger SetPosition("Cleaner");
+            Console.WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
+        }
+    }
+}  
